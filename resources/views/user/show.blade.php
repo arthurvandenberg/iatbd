eb4cb4ac3460faa1e6a67eec1745a9dd3fb98f8b
arthurvandenberg <avdberg95@gmail.com>
2021-03-19 16:15:03 +0100

    wip user profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -16,20 +16,14 @@
         <div class="userProfile__info">
             <div class="userProfile__userInfo">
                 <h1 class="userProfile__title">{{$user->name}} {{$user->lastname}}</h1>
+                <p>Past op: {{$user->preferred_animals}}</p>
             </div>
             <div class="userProfile__description">
-                <p>Past op: {{$user->preferred_animals}}</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam laborum perspiciatis eveniet nostrum voluptas? Commodi veritatis expedita, quibusdam atque totam natus incidunt laudantium sapiente pariatur odit cum similique quod dolor.</p>
-            </div>
-            <div class="userProfile__btnSection">
-                Wil jij dat {{$user->name}} op jouw huisdier past?
-                <a href="#" class="userProfile__button">Stuur een verzoekje!</a>
             </div>
         </div>
     </div>
     <div class="userGallery">
-        @foreach($user->pictures as $picture)
-            <img src="" alt="Yas"/>
-        @endforeach
+        
     </div>
 @endsection
