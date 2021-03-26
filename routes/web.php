e55cd9b8b99db494380f0724117b5318c143ffcc
arthurvandenberg <avdberg95@gmail.com>
2021-03-26 13:28:08 +0100

    feat(reviews): wip reviews

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
 Route::get('/users/{id}', [\App\Http\Controllers\UserController::class, 'show']);
 Route::get('/users/{id}/reviews', [\App\Http\Controllers\ReviewController::class, 'index']);
 Route::get('/users/{id}/reviews/create', [\App\Http\Controllers\ReviewController::class, 'create']);
-Route::get('/users/{id}/reviews/store', [\App\Http\Controllers\ReviewController::class, 'store']);
+Route::post('/users/{id}/reviews/store', [\App\Http\Controllers\ReviewController::class, 'store']);
 
 Route::post('/request/create', [\App\Http\Controllers\RequestController::class, 'store']);
 
