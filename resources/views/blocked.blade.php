ff9b9878923601e92015ad1a4e6222fbd5a39538
arthurvandenberg <avdberg95@gmail.com>
2021-04-08 14:47:37 +0200

    fix(blocked): added translations and fixed space in link

--- resources/views/blocked.blade.php
+++ resources/views/blocked.blade.php
@@ -8,6 +8,6 @@
     <div class="blocked__wrapper">
         <h1 class="blocked__title">:(</h1>
         <p class="blocked__subtitle">{{__('We\'re sorry! It seems your account has been suspended.')}}</p>
-        <p class="blocked__cta">{{__('Do you think this is a mistake?')}}<a class="blocked__cta-link" href="mailto:info@barkplaats.nl?subject='Help! My account has been suspended :('"> {{__('Shoot us an e-mail about it!')}}</a></p>
+        <p class="blocked__cta">{{__('Do you think this is a mistake?')}} <a class="blocked__cta-link" href="mailto:info@barkplaats.nl?subject='Help! My account has been suspended :('">{{__('Shoot us an e-mail about it!')}}</a></p>
     </div>
 @endsection
