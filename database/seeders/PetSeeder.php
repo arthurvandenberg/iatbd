96eb6e1ef22224eb46e38e980f72cea0e0685517
arthurvandenberg <avdberg95@gmail.com>
2021-05-12 11:50:42 +0200

    fix(responsive images): refactored image names and image seeders/migrations

--- database/seeders/PetSeeder.php
+++ database/seeders/PetSeeder.php
@@ -15,16 +15,16 @@ class PetSeeder extends Seeder
     public function run()
     {
         $pets = [
-            ['Bertus', 'Kat', 'Luie kitten die het liefst de hele dag in het zonnetje ligt te slapen.', 'img/pets/bertus_1_1920.jpg', 'img/pets/bertus_1_640.jpg', 'img/pets/bertus_1_1280.jpg', 'img/pets/bertus_1_1920.jpg', 1, false],
-            ['Henk', 'Kat', 'Een beetje een bangerd, maar zodra hij een beetje is opgewarmd een enorm gezellige poes!', 'img/pets/henk_1_1920.jpg', 'img/pets/henk_1_640.jpg', 'img/pets/henk_1_1280.jpg', 'img/pets/henk_1_1920.jpg', 1, false],
-            ['Oscar', 'Kat', 'Bijdehand beest, stiekem wel lief.', 'img/pets/oscar_1_1920.jpg', 'img/pets/oscar_1_640.jpg', 'img/pets/oscar_1_1280.jpg', 'img/pets/oscar_1_1920.jpg', 2, false],
-            ['Boris', 'Hond', 'Blij ei.', 'img/pets/boris_1_1920.jpg', 'img/pets/boris_1_640.jpg', 'img/pets/boris_1_1280.jpg', 'img/pets/boris_1_1920.jpg', 3, false],
-            ['Hamtaro', 'Hamster', 'Dotje, eet meer dan je ooit voor mogelijk zou houden...', 'img/pets/hamtaro_1_1920.jpg', 'img/pets/hamtaro_1_640.jpg', 'img/pets/hamtaro_1_1280.jpg', 'img/pets/hamtaro_1_1920.jpg', 4, false],
-            ['Dave en Ria', 'Cavia', 'Deze twee druiven vreten je arm, pas maar op.', 'img/pets/dave_ria_1_1920.jpg', 'img/pets/dave_ria_1_640.jpg', 'img/pets/dave_ria_1_1280.jpg', 'img/pets/dave_ria_1_1920.jpg', 5, false],
-            ['Nemo', 'Goudvis', 'Goudvis met sterallures, vertelt een hoop grapjes...', 'img/pets/nemo_1_1920.jpg', 'img/pets/nemo_1_640.jpg', 'img/pets/nemo_1_1280.jpg', 'img/pets/nemo_1_1920.jpg', 5, false],
-            ['Swiffer en Pixel', 'Cavia', 'Gezellig stel Cavia\'s, vermaken zich prima samen.' , 'img/pets/pixel_swiffer_1_1920.jpg', 'img/pets/pixel_swiffer_1_640.jpg', 'img/pets/pixel_swiffer_1_1280.jpg', 'img/pets/pixel_swiffer_1_1920.jpg', 6, false],
-            ['Happy', 'Hond', 'De meest blije hond die je ooit tegen gaat komen, geloof je me niet? Just wait and see...', 'img/pets/happy_1_1920.jpg', 'img/pets/happy_1_640.jpg', 'img/pets/happy_1_1280.jpg', 'img/pets/happy_1_1920.jpg', 7, false],
-            ['Pooh', 'Beer', 'Lief beest hoor, maar niet heel slim...', 'img/pets/pooh_1.jpg', 'img/pets/pooh_1.jpg', 'img/pets/pooh_1.jpg', 'img/pets/pooh_1.jpg', 8, true]
+            ['Bertus', 'Kat', 'Luie kitten die het liefst de hele dag in het zonnetje ligt te slapen.', 'img/pets/bertus/bertus_1_1920.jpg', 'img/pets/bertus/bertus_1_640.jpg', 'img/pets/bertus/bertus_1_1280.jpg', 'img/pets/bertus/bertus_1_1920.jpg', 1, false],
+            ['Henk', 'Kat', 'Een beetje een bangerd, maar zodra hij een beetje is opgewarmd een enorm gezellige poes!', 'img/pets/henk/henk_1_1920.jpg', 'img/pets/henk/henk_1_640.jpg', 'img/pets/henk/henk_1_1280.jpg', 'img/pets/henk/henk_1_1920.jpg', 1, false],
+            ['Oscar', 'Kat', 'Bijdehand beest, stiekem wel lief.', 'img/pets/oscar/oscar_1_1920.jpg', 'img/pets/oscar/oscar_1_640.jpg', 'img/pets/oscar/oscar_1_1280.jpg', 'img/pets/oscar/oscar_1_1920.jpg', 2, false],
+            ['Boris', 'Hond', 'Blij ei.', 'img/pets/boris/boris_1_1920.jpg', 'img/pets/boris/boris_1_640.jpg', 'img/pets/boris/boris_1_1280.jpg', 'img/pets/boris/boris_1_1920.jpg', 3, false],
+            ['Hamtaro', 'Hamster', 'Dotje, eet meer dan je ooit voor mogelijk zou houden...', 'img/pets/hamtaro/hamtaro_1_1920.jpg', 'img/pets/hamtaro/hamtaro_1_640.jpg', 'img/pets/hamtaro/hamtaro_1_1280.jpg', 'img/pets/hamtaro/hamtaro_1_1920.jpg', 4, false],
+            ['Dave en Ria', 'Cavia', 'Deze twee druiven vreten je arm, pas maar op.', 'img/pets/dave_ria/dave_ria_1_1920.jpg', 'img/pets/dave_ria/dave_ria_1_640.jpg', 'img/pets/dave_ria/dave_ria_1_1280.jpg', 'img/pets/dave_ria/dave_ria_1_1920.jpg', 5, false],
+            ['Nemo', 'Goudvis', 'Goudvis met sterallures, vertelt een hoop grapjes...', 'img/pets/nemo/nemo_1_1920.jpg', 'img/pets/nemo/nemo_1_640.jpg', 'img/pets/nemo/nemo_1_1280.jpg', 'img/pets/nemo/nemo_1_1920.jpg', 5, false],
+            ['Swiffer en Pixel', 'Cavia', 'Gezellig stel Cavia\'s, vermaken zich prima samen.' , 'img/pets/pixel_swiffer/pixel_swiffer_1_1920.jpg', 'img/pets/pixel_swiffer/pixel_swiffer_1_640.jpg', 'img/pets/pixel_swiffer/pixel_swiffer_1_1280.jpg', 'img/pets/pixel_swiffer/pixel_swiffer_1_1920.jpg', 6, false],
+            ['Happy', 'Hond', 'De meest blije hond die je ooit tegen gaat komen, geloof je me niet? Just wait and see...', 'img/pets/happy/happy_1_1920.jpg', 'img/pets/happy/happy_1_640.jpg', 'img/pets/happy/happy_1_1280.jpg', 'img/pets/happy/happy_1_1920.jpg', 7, false],
+            ['Pooh', 'Beer', 'Lief beest hoor, maar niet heel slim...', 'img/pets/pooh/pooh_1.jpg', 'img/pets/pooh/pooh_1.jpg', 'img/pets/pooh/pooh_1.jpg', 'img/pets/pooh/pooh_1.jpg', 8, true]
         ];
 
         foreach($pets as $pet){
